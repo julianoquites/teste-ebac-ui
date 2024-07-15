eab669d3f5c98f7541ceae485f856cc0f90adb68
Juliano Quites <juliano.quites@gmail.com>
2024-07-15 17:34:31 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,42 @@
 /// <reference types="cypress" />;
 
 describe("Funcionalidade: Login", () => {
-    it("Deve fazer login com sucesso", () => {
-        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
-        cy.get("#username").type("jq.teste@teste.com.br");
-        cy.get("#password").type("kabazz123");
-        cy.get(".woocommerce-form > .button").click();
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+  });
 
-        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
-          "contain",
-          "Olá, jq.teste (não é jq.teste? Sair)"
-        );
+    afterEach(() => {
+      cy.screenshot();
+    });
+
+  it("Deve fazer login com sucesso", () => {
+    cy.get("#username").type("jq.teste@teste.com.br");
+    cy.get("#password").type("kabazz123");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+      "contain",
+      "Olá, jq.teste (não é jq.teste? Sair)"
+    );
+  });
+  it("Deve exibir uma mensagem de erro ao inserir usuário inválido", () => {
+    cy.get("#username").type("jujujujuju@teste.com.br");
+    cy.get("#password").type("kabazz123");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-error").should(
+      "contain",
+      "Endereço de e-mail desconhecido"
+    );
+    cy.get(".woocommerce-error").should("exist");
+  });
+
+  it("Deve exibir uma mensagem de erro ao inserir senha inválida", () => {
+    cy.get("#username").type("jq.teste@teste.com.br");
+    cy.get("#password").type("oioi123");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-error").should(
+      "contain",
+      "Erro: A senha fornecida para o e-mail jq.teste@teste.com.br está incorreta. Perdeu a senha?"
+    );
+    cy.get(".woocommerce-error").should("exist");
   });
 });
